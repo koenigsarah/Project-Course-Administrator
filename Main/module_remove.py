c99114080d6352850622b61223277c379a0df4fd
kazy <katharina.esterhazy@stud.fh-campuswien.ac.at>
2023-05-07 09:23:53 +0200

    ToDo: Ablauf des Programms hinzugefügt

--- Main/module_remove.py
+++ Main/module_remove.py
@@ -1,1 +1,9 @@
-def remove_person()
+personendaten = {"id" : 51802222, "rolle" : "Teilnehmer", "vorname" : "Sarah", "nachname" : "König", "straße" : "1.A Straße", "hausnummer" : "4", "plz" : "4425", "stadt" : "Waidhofen/Ybbs"}
+
+
+def remove_person(personen):
+    person = input("Welche Person willst du löschen? ", personen)
+    # liste formatieren: alle vnamen nnamen ids untereinander
+    # nur id eingeben? ja, weil doppelte vor- oder nachnamen...
+    # nachfragen ob tatsächlich löschen j n dann erst
+    # wenn ja -> löschen, wenn nein -> zurück zum hauptmenü (was möchtest du machen?)

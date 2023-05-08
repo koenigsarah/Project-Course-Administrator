6e73cfe3d1ba6665f558e9a387a99357eb80a3c2
sk199 <sk1997@gmx.net>
2023-05-08 10:31:55 +0200

    Main geändert

--- Main/00_main.py
+++ Main/00_main.py
@@ -4,22 +4,23 @@
 # import module_change
 # import module_remove
 
-personendaten = {"id" : 51802222, "rolle" : "Teilnehmer", "vorname" : "Sarah", "nachname" : "König", "straße" : "1.A Straße", "hausnummer" : "4", "plz" : "4425", "stadt" : "Waidhofen/Ybbs"}
+personendaten = {"id" : [1802222], "rolle" : ["Teilnehmer"], "vorname" : ["Sarah"], "nachname" : ["König"], "straße" : ["1.A Straße"], "hausnummer" : ["4"], "plz" : ["4425"], "stadt" : "[Waidhofen/Ybbs]"}
 
 ## für Rolle gibt es nur zwei Werte, kann man das irgendiwe darstellen? Liste macht hier keinen Sinn, da sonst zwei Werte bei mir wären
 
 def hauptmenü():
-    start = input("Hallo, heute ist ein guter Tag um eine Liste zu bearbeiten. Gib 'a' ein, wenn du Personen hinzufügen möchtest, gibt 'c' ein, wenn Du Personen ändern möchtest und gib r ein, wenn Du Personen entfernen möchtest. ")
+    start = input("Hallo, heute ist ein guter Tag um eine Liste zu bearbeiten. Gib 'a' für Hinzufügen ein, 'c' fürs Ändern und 'r' fürs Entfernen. ")
     if start == ["a"]:
         #start mit module_add
     elif start == ["c"]:
         #start mit module_change
-    elif start =? ["r"]:
+    elif start == ["r"]:
         #srat mit module_remove
     else:
         try:
 
-#MOdule so verknüfen, dass man sie mit jeweiligen Buchstaben aufrufen kann (wenn das aufgerufen ist, wird diese Funktion ausgeführt. if a, kommt Modul_add ins Spiel, try and error kommt hier hinzu (ist es wirklich gültiger Buchstabe - noch warten
+#Module so verknüfen, dass man sie mit jeweiligen Buchstaben aufrufen kann (wenn das aufgerufen ist, wird diese Funktion ausgeführt. if a, kommt Modul_add ins Spiel, try and error kommt hier hinzu (ist es wirklich gültiger Buchstabe - noch warten
+# Gib "Main" ein, wenn Du zum Hauptmenü willst
 
 hauptmenü()
 
--- Main/module_add.py
+++ Main/module_add.py
@@ -3,8 +3,7 @@
 a = input("Sag a: ")
 
 def add_person():
-    if a == "a":
-        b = input("Bitte sag mir die id (Matrikelnummer, Personalnummer) von der Person, die du hinzufügen möchtest: ")
+    b = input("Bitte sag mir die id Matrikelnummer von der Person, die du hinzufügen möchtest: ")
 
 #wie hier Daten eingeben, nacheinander?
 

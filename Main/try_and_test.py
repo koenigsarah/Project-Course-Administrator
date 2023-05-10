228115ba70872805d1336486e8d5f5d7169b8e2f
kazy <katharina.esterhazy@stud.fh-campuswien.ac.at>
2023-05-10 13:02:07 +0200

    WS

--- Main/try_and_test.py
+++ Main/try_and_test.py
@@ -6,3 +6,13 @@ if (0 > 1):
     print("Nein")
 else:
     print("Hallo Katharina")
+
+with open("personen.txt", "r", encoding="UTF-8") as file:
+    print(file.read())
+
+with open("personen.txt", "w", encoding="UTF-8") as file:
+    file.write("01, Alex, Berger, Burggasse 3, 1070, Wien\n")
+    file.write(input())
+    file.write("\n03, Otto, Berger, Burggasse 3, 1070, Wien\n")
+with open("personen.txt", "r", encoding="UTF-8") as file:
+    print(file.read())

61831cbae374756b7305c76ca001ab493b25179b
koenigsarah <sk1997@gmx.net>
2023-05-11 12:47:11 +0200

    x

--- Main/module_display_persons.py
+++ Main/module_display_persons.py
@@ -1,5 +1,4 @@
 
-
 t_daten = {"id": [1, 2, 3, 4, 5],
              "rolle": ["Teilnehmer*in"],
              "vname": ["Sarah", "Anna", "Otto", "Ella", "Lia"],
@@ -9,8 +8,6 @@ t_daten = {"id": [1, 2, 3, 4, 5],
              "plz": [4425, 1030, 1200, 1230, 1160],
              "ort": ["Waidhofen/Ybbs", "Wien", "Wien", "Wien", "Wien"]}
 
-
-
 v_daten = {"id": [1, 2, 3, 4, 5],
              "rolle": ["Lektor*in"],
              "vname": ["Sarah", "Anna", "Otto", "Ella", "Lia"],
@@ -19,7 +16,6 @@ v_daten = {"id": [1, 2, 3, 4, 5],
              "nr": [3, 5, 77, 23, 9],
              "plz": [4425, 1030, 1200, 1230, 1160],
              "ort": ["Waidhofen/Ybbs", "Wien", "Wien", "Wien", "Wien"]}
-
 
 def display():
 
